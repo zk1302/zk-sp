7b08c8986a4c9a302ef150394bab60772d6ec429
zk1302 <984044821@qq.com>
2022-05-30 11:13:38 +0800

    init commit

---
+++ src/main/java/com/zk/sp/ZkspApplication.java
@@ -0,0 +1,13 @@
+package com.zk.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zk/sp/ZkspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zk.sp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
